24042d15bccda8c30cfff732b97252de814f03a7
DaMangledMan <113638676+DaMangledMan@users.noreply.github.com>
2022-10-18 15:38:42 -0600

    Word Class finished

--- Word.cs
+++ Word.cs
@@ -1,17 +1,16 @@
-/*
-    Word_Class:
-        Will store the current word that needs to be guessed.
-        Methods: 
-            setter:
-                to set the word to something random or a user input
-            getter:
-                to get the set word
-*/
 
+// Word_Class:
 class Word{
+    Random random = new Random ();
+
+
+    // Will store the current word that needs to be guessed.
     List <string> word = new List <string> ();
 
-
+    // Methods: 
+    
+    // setter:
+        // to set the word to something random or a user input
     public void Setter()
     {
         string fullWord = "";
@@ -27,44 +26,23 @@ class Word{
             }
             else if (input == "2")
             {
-                List <string> possibleWords = []
+                List <string> possibleWords = new List<string> {"climate", "dragon", "likely", "wriggle", "general", "total", "fibre", "credit", "laborer", "diagram", "slow", "reveal", "overeat", "salt", "horn", "glide", "grain", "inquiry", "rebel", "charm", "loot", "visible", "cope", "moving", "remark", "dictate", "sausage", "session", "family", "bike", "ballot"};
+                fullWord = possibleWords[random.Next(possibleWords.Count)];
             }
         } while (true);
+        var char_word = fullWord.ToCharArray();
+        List<string> wordList = new List<string> ();
+        for (int i = 0; i == char_word.Length; i += 1)
+        {
+            wordList.Add(char_word[i].ToString());
+        }
+        this.word = wordList;
+    }
 
+    // getter:
+        // to get the set word
+    public List<string> Getter()
+    {
+        return word;
     }
 }
-
-
-
-
-climate
-dragon
-likely
-wriggle
-general
-total
-fibre
-credit
-laborer
-diagram
-slow
-reveal
-overeat
-salt
-horn
-glide
-grain
-inquiry
-rebel
-charm
-loot
-visible
-cope
-moving
-remark
-dictate
-sausage
-session
-family
-bike
-ballot
